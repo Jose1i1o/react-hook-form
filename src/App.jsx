c50f194200c0aefe2d9797c3d8a35436a8605497
Jose Valenzuela <guadix88@gmail.com>
2022-08-05 01:38:46 +0200

    feat(base code): add basic code for live-coding

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useForm } from "react-hook-form";
+import Header from "./ui/Header/Header";
+
+let renderedCount = 0;
+export const App = () => {
+
+  renderedCount++;
+
+  return (
+    <>
+      <Header renderedCount={renderedCount}/>
+      <h1>Login</h1>
+      <form>
+        <input name='firstName' placeholder="First name" />
+        <input name='hobbies' placeholder="i.e. coding every day 🤩" />
+        <input name='favourite series' placeholder="Breaking bad" />
+        <input type="submit" />
+      </form>
+    </>
+  );
+}
